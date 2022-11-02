b4277185181d89dbf59fe7a472eb2c344fec20ae
Pandudaniel <pandudaniel@student.telkomuniversity.ac.id>
2022-11-02 16:34:55 +0700

    MODUL 1

---
+++ MODUL 1/Menu.java
@@ -0,0 +1,7 @@
+public class Menu {
+    String Name;
+    int Category;
+    double price;
+    // TODO Create Attribute of Menu; Name, Category, and Price then Create Setter
+
+}
---
+++ MODUL 1/User.java
@@ -0,0 +1,21 @@
+public class User {
+
+    // TODO Create Attribute of User; Name and Phone Number then Create Setter
+    String nama;
+    int nohandphone;
+    int kelas;
+
+    // TODO Create Method to Register User and Display User's Name and Phone Number
+    // and success message
+    public String getName() {
+        return nama;
+    }
+
+    public int getKelas() {
+        return kelas;
+    }
+
+    public int getNoHP() {
+        return nohandphone;
+    }
+}
